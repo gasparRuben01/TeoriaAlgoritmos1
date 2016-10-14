ec4aa9ce5ad6a21d72674ef446c2c62ee765fb0c
Diego Almirón R <diego90dionisio@gmail.com>
2016-10-14 15:03:45 -0300

    Agrego graficos

---
+++ estadistico de orden k/graficos/sorted.m
@@ -0,0 +1,28 @@
+Hf = figure(1)
+
+set(Hf,'PaperUnits','inches','PaperPosition',[0 0 3.5 2.5])
+
+Ha = axes
+
+set(Ha,'Box','on','FontName','Arial','FontSize',8,'GridLineStyle','--','LineWidth',1,'TickDir','in');
+grid on
+hold on
+
+SIM = dlmread("sorted_selection.txt","\t",1,0);
+
+plot(Ha,SIM(:,1),1000*SIM(:,2),'-bo','linewidth',3,'markersize',4,'markerfacecolor','b');
+plot(Ha,SIM(:,1),1000*SIM(:,3),'-ro','linewidth',2,'MarkerSize',3,'makerfacecolor','r');
+plot(Ha,SIM(:,1),1000*SIM(:,4),'-go','linewidth',1,'markersize',2,'markerfacecolor','g');
+axis([0 100000 0 200]);
+
+ylabel('Tiempo [ms]');
+xlabel('Cantidad de elemento [n]');
+
+Hleg = legend(Ha,'Estadistico orden k ,k = 0','Estadistico orden k ,k = n/2','Estadistico orden k ,k = n',"location",'northwest');
+legend('boxoff');
+set(Hleg,'FontName','Arial','FontSize',8);
+
+print('sorted_selection.png','-dpng','-r300');
+
+return
+
