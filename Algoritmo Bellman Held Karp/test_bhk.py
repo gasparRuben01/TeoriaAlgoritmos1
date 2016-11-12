7d0c96b13cec1ff83f30ea0ef1a52f2fa926314d
joaquinbf <joaquinbf@live.com>
2016-11-12 16:35:14 -0300

    agregue nuevas pruebas y test_bhk.py ahora prueba cada uno de los grafos y emite un informe de los tiempos, costos minimos y caminos minimos encontrados

--- Algoritmo Bellman Held Karp/test_bhk.py
+++ Algoritmo Bellman Held Karp/test_bhk.py
@@ -1,7 +1,8 @@
 from sys import *
 from bhk import *
+from time import *
 
-def read_file(name_file, N):
+def read_file(name_file):
     to_read = open(name_file)
     matrix = []
     for line in to_read:
@@ -12,8 +13,28 @@ def read_file(name_file, N):
     to_read.close()
     return matrix
 
-name_file, N = argv[1], int(argv[2])
-G = read_file(name_file, N)
-cost, path = bhk(G, N)
-print cost
-print path
+def print_result(delta_time, N, cost, path):
+	str_time = str(delta_time)
+	str_N = str(N)
+	str_cost = str(cost)
+	str_path = [str(v) for v in path]
+	sep = "-"
+	str_path = sep.join(str_path) 
+	sep = ","
+	str_result = sep.join([str_time, str_N, str_cost, str_path])
+	print str_result
+
+def test_tsp(G, N):
+	init = time()
+	cost, path = bhk(G, N)
+	delta_time = time() - init
+	print_result(delta_time, N, cost, path)
+
+size_graph = [4,15,17,26,42,48]
+print "time,size,cost_min,path_min"
+for N in size_graph:
+	name_file = "graph_" + str(N) + "_vertex.txt"
+	G = read_file(name_file)
+	test_tsp(G, N)
+
+
