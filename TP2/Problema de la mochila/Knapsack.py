da07c12ceec2cd7460fbeef1bc7bd0fa3f5b4f7e
Gaspar <gaspar.zanellini@gmail.com>
2016-11-16 17:35:56 -0300

    mochila

--- TP2/Problema de la mochila/Knapsack.py
+++ TP2/Problema de la mochila/Knapsack.py
@@ -6,22 +6,24 @@ class Knapsack:
 		i=len(objects)
 		sub_capacity=self.capacity
 		while (i>0 and sub_capacity>0):
-			object_value=objects[i-1].get_value()
-			object_weigth=objects[i-1].get_weigth()
-			if object_weigth<sub_capacity and not max(memo[i-1][sub_capacity], object_value+memo[i-1][sub_capacity-object_weigth])==memo[i-1][sub_capacity]:
-				self.knapsack_elements.append(objects[i-1])
+			k=i-1
+			object_value=objects[k].get_value()
+			object_weigth=objects[k].get_weigth()
+			if object_weigth<=sub_capacity and memo[i-1][sub_capacity]<object_value+memo[i-1][sub_capacity-object_weigth]:
+				self.knapsack_elements.append(objects[k])
 				sub_capacity-=object_weigth
 			i-=1			
 
 	def __init__(self, capacity, objects):
 		self.capacity=capacity
-		memo=[[0 for i in range(0, capacity+1)] for i in range(0, len(objects)+1)]
-		for i in range(1, len(objects)):
-			for sub_capacity in range(1, capacity):
-					if objects[i].get_weigth() > sub_capacity:
+		memo=[[0]*(capacity+1) for i in range(len(objects)+1)]
+		for i in range(1, len(objects)+1):
+			k=i-1
+			for sub_capacity in range(1, capacity+1):
+					if objects[k].get_weigth() > sub_capacity:
 						memo[i][sub_capacity]=memo[i-1][sub_capacity]
 					else:
-						memo[i][sub_capacity]=max(memo[i-1][sub_capacity], objects[i].get_value()+memo[i-1][sub_capacity-objects[i].get_weigth()])
+						memo[i][sub_capacity]=max(memo[i-1][sub_capacity], objects[k].get_value()+memo[i-1][sub_capacity-objects[k].get_weigth()])
 		self.value=memo[len(objects)][capacity]
 		self.knapsack_elements=[]
 		self.fill_knapsack(memo, objects)
