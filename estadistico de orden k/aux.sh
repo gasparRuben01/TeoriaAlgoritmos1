90834c1966996a0213a340a08317388ac0434c25
Diego Almirón R <diego90dionisio@gmail.com>
2016-11-22 09:41:33 -0300

    Datos

---
+++ estadistico de orden k/aux.sh
@@ -0,0 +1,4 @@
+python test_time.py aux/86000a.txt >> tiemposParaGraficar.txt && \
+    python test_time.py aux/90000a.txt >> tiemposParaGraficar.txt && \
+    python test_time.py aux/94000a.txt >> tiemposParaGraficar.txt && \
+    python test_time.py aux/int_rand.txt >> tiemposParaGraficar.txt 
